d172ea1884d833f2edeedae088967240ed45e715
Mikhail Obodnikov <mike@obodnikov.com>
2025-09-24 16:29:25 +0000

    Upload New File

---
+++ localization/hungarian_localization.php
@@ -0,0 +1,24 @@
+<?php
+// File: localization/hu_HU.inc
+// Hungarian localization for LLM Assistant Plugin
+
+$labels = array();
+$labels['ai_assistant'] = 'AI Asszisztens';
+$labels['toggle_assistant'] = 'AI Asszisztens ki/be';
+$labels['help_reply'] = 'Válasz Segítség';
+$labels['help_compose'] = 'Írás Segítség';
+$labels['improve_text'] = 'Szöveg Javítása';
+$labels['summarize'] = 'Összegzés';
+$labels['enter_prompt'] = 'Írja be a kérését...';
+$labels['additional_context'] = 'További kontextus (opcionális)...';
+$labels['generate'] = 'Generálás';
+$labels['insert_response'] = 'Válasz Beszúrása';
+$labels['generated_response'] = 'Generált Válasz:';
+$labels['generating'] = 'Válasz generálása...';
+$labels['llm_assistant_config'] = 'AI Asszisztens Beállítások';
+
+$messages = array();
+$messages['error_empty_prompt'] = 'Kérjük, adjon meg egy kérést';
+$messages['error_api_call'] = 'API hívás sikertelen';
+
+?>
