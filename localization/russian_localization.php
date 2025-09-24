03b485b2c259d4842360e80b2a2cec3cd9dd7d7b
Mikhail Obodnikov <mike@obodnikov.com>
2025-09-24 16:28:24 +0000

    Upload New File

---
+++ localization/russian_localization.php
@@ -0,0 +1,24 @@
+<?php
+// File: localization/ru_RU.inc
+// Russian localization for LLM Assistant Plugin
+
+$labels = array();
+$labels['ai_assistant'] = 'ИИ Помощник';
+$labels['toggle_assistant'] = 'Переключить ИИ Помощника';
+$labels['help_reply'] = 'Помочь с Ответом';
+$labels['help_compose'] = 'Помочь с Написанием';
+$labels['improve_text'] = 'Улучшить Текст';
+$labels['summarize'] = 'Резюмировать';
+$labels['enter_prompt'] = 'Введите ваш запрос...';
+$labels['additional_context'] = 'Дополнительный контекст (необязательно)...';
+$labels['generate'] = 'Генерировать';
+$labels['insert_response'] = 'Вставить Ответ';
+$labels['generated_response'] = 'Сгенерированный Ответ:';
+$labels['generating'] = 'Генерация ответа...';
+$labels['llm_assistant_config'] = 'Настройки ИИ Помощника';
+
+$messages = array();
+$messages['error_empty_prompt'] = 'Пожалуйста, введите запрос';
+$messages['error_api_call'] = 'Ошибка вызова API';
+
+?>
